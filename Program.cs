4a856bfb10b6e30264de31c0403de177ca95e5b1
GNW-GNW <102678939+GNW-GNW@users.noreply.github.com>
2022-04-01 11:23:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+
+switch (a)
+{
+    case 0:Console.WriteLine("Zero");
+        break;
+    case 1:Console.WriteLine("one");
+        break;
+    case 2:Console.WriteLine("two");
+        break;
+    default:Console.WriteLine("Fuck you");
+        break;
+}
+
